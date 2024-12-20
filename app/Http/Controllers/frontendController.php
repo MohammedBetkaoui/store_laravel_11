ff8e1601e34f23c5197cd1f25209b9347b8ec75a
MohammedBetkaoui <mohammed.betkaoui@univ-bba.dz>
2024-12-21 00:31:40 +0100

    Description des modifications effectuées

--- app/Http/Controllers/frontendController.php
+++ app/Http/Controllers/frontendController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Carbon\Carbon;
 
 class frontendController extends Controller
 {
     public function home(){
-        return view('frontend.index');
+
+    //     $user = new User();
+    //     $user->name        = 'Administrator';
+    //     $user->email       = 'admin@gmail.com';
+    //     $user->password    = Hash::make('123456789000');
+    //     $user->created_at  =Carbon::now();
+
+    //     $user->save();
+    //     $user->assignRole('admin'); 
+    //   return $user;
+         return view('frontend.index');
     }
 }
