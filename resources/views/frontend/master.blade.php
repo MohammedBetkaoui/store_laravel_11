76ad8adb5a004ef7d71b8bc14c17ef28a671493a
MohammedBetkaoui <mohammed.betkaoui@univ-bba.dz>
2024-12-31 20:22:10 +0100

    update

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -15,6 +15,8 @@
 <link rel="stylesheet" type="text/css" href="{{asset('/styles/main_styles.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('/styles/responsive.css')}}">
 
+
+
 </head>
 
 <body>
